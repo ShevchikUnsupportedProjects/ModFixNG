1cdfc51e235ac7f4054eaf857154b7225f6c5740
Shevchik <Shev4ik.den@gmail.com>
2014-01-04 00:26:59 +0400

    Fix compilation

--- src/modfixng/FixFreecamBlocks.java
+++ src/modfixng/FixFreecamBlocks.java
@@ -47,7 +47,6 @@ public class FixFreecamBlocks implements Listener {
 		//zeroItemsCheck
 		initInvCheck();
 		//forceCloseInv
-		initOpenInventoryFixListener();
 		initClientCloseInventoryFixListener();
 		initServerCloseInventoryFixListener();
 		initBlockCheck();
