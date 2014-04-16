a34ea797fd3be9e76997b7608cc855672640a0f8
Shevchik <Shev4ik.den@gmail.com>
2014-04-16 23:30:09 +0400

    Remove unused import

--- src/modfixng/fixes/ValidateActions.java
+++ src/modfixng/fixes/ValidateActions.java
@@ -28,7 +28,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
