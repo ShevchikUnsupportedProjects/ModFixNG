626e10d11274a8f22d24c4751f4103becdd6ca66
Shevchik <Shev4ik.den@gmail.com>
2014-05-20 19:27:18 +0400

    Add a wrapper to properly get/set hashsets

--- src/modfixng/main/Config.java
+++ src/modfixng/main/Config.java
@@ -21,8 +21,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 public class Config {
@@ -60,7 +60,7 @@ public class Config {
 	public int microblockFixBlockID = -1;
 
 	public void loadConfig() {
-		FileConfiguration config = YamlConfiguration.loadConfiguration(configfile);
+		YamlConfigurationWrapper config = YamlConfigurationWrapper.loadConfiguration(configfile);
 
 		fixBagEnabled = config.getBoolean("BackPackFix.enabled", fixBagEnabled);
 		fixBag19ButtonClickEnabled = config.getBoolean("BackPackFix.restrict19ButtonClick.enabled", fixBag19ButtonClickEnabled);
@@ -69,20 +69,20 @@ public class Config {
 		fixBagToolboxFixEnabled = config.getBoolean("BackPackFix.fixToolbox.enabled", fixBagToolboxFixEnabled);
 
 		fixFreecamBlockCloseInventoryOnBreakCheckEnabled = config.getBoolean("ProperlyCloseInventories.checkBlocks.enabled", fixFreecamBlockCloseInventoryOnBreakCheckEnabled);
-		fixFreecamBlockCloseInventoryOnBreakCheckBlocksIDs = new HashSet<String>(config.getStringList("ProperlyCloseInventories.checkBlocks.IDs"));
+		fixFreecamBlockCloseInventoryOnBreakCheckBlocksIDs = config.getHashSet(String.class, "ProperlyCloseInventories.checkBlocks.IDs");
 
 		fixFreecamEntitiesEnabled = config.getBoolean("ProperlyCloseInventories.checkEntities.enabled", fixFreecamEntitiesEnabled);
-		fixFreecamEntitiesEntitiesIDs = new HashSet<Short>(config.getShortList("ProperlyCloseInventories.checkEntities.IDs"));
+		fixFreecamEntitiesEntitiesIDs = config.getHashSet(Short.class, "ProperlyCloseInventories.checkEntities.IDs");
 
 		validateActionsEnabled = config.getBoolean("ValidateActions.enabled", validateActionsEnabled);
 
 		fixSlotDesyncEnabled = config.getBoolean("ForceSyncSlots.enabled", fixSlotDesyncEnabled);
 
 		restrict19Enabled = config.getBoolean("Restrict19ButtonClick.enabled", restrict19Enabled);
-		restrict19InvetoryNames = new HashSet<String>(config.getStringList("Restrict19ButtonClick.inventoryNames"));
+		restrict19InvetoryNames = config.getHashSet(String.class, "Restrict19ButtonClick.inventoryNames");
 
 		restrictShiftEnabled = config.getBoolean("RestrictShiftButtonClick.enabled", restrictShiftEnabled);
-		restrictShiftInvetoryNames = new HashSet<String>(config.getStringList("RestrictShiftButtonClick.inventoryNames"));
+		restrictShiftInvetoryNames = config.getHashSet(String.class, "RestrictShiftButtonClick.inventoryNames");
 
 		microblockFixEnabled = config.getBoolean("MicroblockFix.enabled", microblockFixEnabled);
 		microblockFixItemID = config.getInt("MicroblockFix.itemID", microblockFixItemID);
@@ -92,29 +92,29 @@ public class Config {
 	}
 
 	public void saveConfig() {
-		FileConfiguration config = new YamlConfiguration();
+		YamlConfigurationWrapper config = new YamlConfigurationWrapper();
 
 		config.set("BackPackFix.enabled", fixBagEnabled);
 		config.set("BackPackFix.restrict19ButtonClick.enabled", fixBag19ButtonClickEnabled);
-		config.set("BackPackFix.restrict19ButtonClick.inventoryNames", new ArrayList<String>(fixBag19ButtonClickBagInventoryNames));
+		config.set("BackPackFix.restrict19ButtonClick.inventoryNames", fixBag19ButtonClickBagInventoryNames);
 		config.set("BackPackFix.fixCropanalyzer.enabled", fixBagCropanalyzerFixEnabled);
 		config.set("BackPackFix.fixToolbox.enabled", fixBagToolboxFixEnabled);
 
 		config.set("ProperlyCloseInventories.checkBlocks.enabled", fixFreecamBlockCloseInventoryOnBreakCheckEnabled);
-		config.set("ProperlyCloseInventories.checkBlocks.IDs", new ArrayList<String>(fixFreecamBlockCloseInventoryOnBreakCheckBlocksIDs));
+		config.set("ProperlyCloseInventories.checkBlocks.IDs", fixFreecamBlockCloseInventoryOnBreakCheckBlocksIDs);
 
 		config.set("ProperlyCloseInventories.checkEntities.enabled", fixFreecamEntitiesEnabled);
-		config.set("ProperlyCloseInventories.checkEntities.IDs", new ArrayList<Short>(fixFreecamEntitiesEntitiesIDs));
+		config.set("ProperlyCloseInventories.checkEntities.IDs", fixFreecamEntitiesEntitiesIDs);
 
 		config.set("ValidateActions.enabled", validateActionsEnabled);
 
 		config.set("ForceSyncSlots.enabled", fixSlotDesyncEnabled);
 
 		config.set("Restrict19ButtonClick.enabled", restrict19Enabled);
-		config.set("Restrict19ButtonClick.inventoryNames", new ArrayList<String>(restrict19InvetoryNames));
+		config.set("Restrict19ButtonClick.inventoryNames", restrict19InvetoryNames);
 
 		config.set("RestrictShiftButtonClick.enabled", restrictShiftEnabled);
-		config.set("RestrictShiftButtonClick.inventoryNames", new ArrayList<String>(restrictShiftInvetoryNames));
+		config.set("RestrictShiftButtonClick.inventoryNames", restrictShiftInvetoryNames);
 
 		config.set("MicroblockFix.enabled", microblockFixEnabled);
 		config.set("MicroblockFix.itemID", microblockFixItemID);
@@ -125,4 +125,42 @@ public class Config {
 		} catch (IOException e) {
 		}
 	}
+
+	private static class YamlConfigurationWrapper extends YamlConfiguration {
+
+		public static YamlConfigurationWrapper loadConfiguration(File file) {
+			YamlConfigurationWrapper wrapper = new YamlConfigurationWrapper();
+			try {
+				wrapper.load(file);
+			} catch (Exception e) {
+			}
+			return wrapper;
+		}
+
+		@SuppressWarnings("unchecked")
+		private <T> HashSet<T> getHashSet(Class<T> t, String path) {
+			try {
+				HashSet<T> set = new HashSet<T>();
+				List<?> list = getList(path, new ArrayList<T>());
+				for (Object element : list) {
+					set.add((T) element);
+				}
+				return set;
+			} catch (Exception e) {
+			}
+			return new HashSet<T>();
+		}
+
+		@Override
+		public void set(String path, Object obj) {
+			if (obj instanceof HashSet) {
+				ArrayList<Object> list = new ArrayList<Object>((HashSet<?>) obj);
+				set(path, list);
+			} else {
+				super.set(path, obj);
+			}
+		}
+
+	}
+
 }
