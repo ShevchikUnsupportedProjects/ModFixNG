5569f6fdb8f25eb8f81ba9ee03c5f4140668e970
Shevchik <Shev4ik.den@gmail.com>
2014-08-10 18:37:28 +0400

    Add support for 1.6.4

---
+++ src/modfixng/packets/v1_6_R3/PacketHook.java
@@ -0,0 +1,44 @@
+package modfixng.packets.v1_6_R3;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+import modfixng.packets.PacketHookInterface;
+import net.minecraft.server.v1_6_R3.Packet;
+
+public class PacketHook implements PacketHookInterface {
+
+	@Override
+	public void initInBlockDigListener() {
+		hookPacket(CloseInventory.class, CloseInventory.getPacketID());
+	}
+
+	@Override
+	public void initInCloseInventoryListener() {
+		hookPacket(CloseInventory.class, CloseInventory.getPacketID());
+	}
+
+	@Override
+	public void initInClickInventoryListener() {
+		hookPacket(InventoryClick.class, InventoryClick.getPacketID());
+	}
+
+	@SuppressWarnings("unchecked")
+	private void hookPacket(Class<?> packetclass, int packetid) {
+		Packet.l.a(packetid, packetclass);
+		Field[] packetFields = Packet.class.getDeclaredFields();
+		for (Field field : packetFields) {
+			try {
+				if (Map.class.isAssignableFrom(field.getType())) {
+					field.setAccessible(true);
+					@SuppressWarnings({ "rawtypes" })
+					Map packets = (Map) field.get(null);
+					packets.put(packetclass, packetid);
+				}
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+}
